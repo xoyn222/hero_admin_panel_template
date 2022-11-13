369934afae7901a2e3ceecbc104004f8ce22f5e5
Artem <117912625+xoyn222@users.noreply.github.com>
2022-11-13 10:28:57 +0000

    Update HeroesList.js

--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -6,11 +6,6 @@ import { heroesFetching, heroesFetched, heroesFetchingError } from '../../action
 import HeroesListItem from "../heroesListItem/HeroesListItem";
 import Spinner from '../spinner/Spinner';
 
-// Задача для этого компонента:
-// При клике на "крестик" идет удаление персонажа из общего состояния
-// Усложненная задача:
-// Удаление идет и с json файла при помощи метода DELETE
-
 const HeroesList = () => {
     const {heroes, heroesLoadingStatus} = useSelector(state => state);
     const dispatch = useDispatch();
